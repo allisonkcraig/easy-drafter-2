35f8de014abed7a9720de2ee7d624dc77bebe682
Allison  Craig <allisoncraig@mac.lan>
2025-10-01 08:21:28 -0400

    stub out first step of top block creation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../style/Home.css';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   const [hover, setHover] = useState(false);
@@ -7,13 +8,15 @@ const Home: React.FC = () => {
     <div>
       <h1>Welcome to Easy Drafter 2.0</h1>
       <p>Please select which block you would like to draft:</p>
-      <img
-        className="Home-block"
-        src={hover ? 'top-p.png' : 'top.png'}
-        alt="image of a pattern block for a blouse"
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      />
+      <Link to="/top">
+        <img
+          className="Home-block"
+          src={hover ? 'top-p.png' : 'top.png'}
+          alt="image of a pattern block for a blouse"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        />
+      </Link>
       {/* TODO: add image of skirt pattern blocks that navigate users to the beginning stages of block creation */}
     </div>
   );
---
+++ src/components/TopBlockStep1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar: React.FC = () => {
+  return (
+    <div>
+      {/* TODO: on this page we will gather two measurements to use to determine base size */}
+    </div>
+  );
+};
+
+export default NavBar;
