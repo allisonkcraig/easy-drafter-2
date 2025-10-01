f0c16851104cb0c85f60077c4494276f63776c88
Allison  Craig <allisoncraig@mac.lan>
2025-09-30 21:08:38 -0400

    add blouse block PNG which will be used for the routing to different pages to draft blocks

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import '../style/NavBar.css';
+import React, { useState } from 'react';
+import '../style/Home.css';
 
 const Home: React.FC = () => {
+  const [hover, setHover] = useState(false);
   return (
     <div>
       <h1>Welcome to Easy Drafter 2.0</h1>
       <p>Please select which block you would like to draft:</p>
-      {/* TODO: add images of pattern blocks that navigate users to the beginning stages of block creation */}
+      <img
+        className="Home-block"
+        src={hover ? 'top-p.png' : 'top.png'}
+        alt="image of a pattern block for a blouse"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      />
+      {/* TODO: add image of skirt pattern blocks that navigate users to the beginning stages of block creation */}
     </div>
   );
 };
